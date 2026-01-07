6409c25b932cfb83fd73c3e6a3c7be8b7dead39d
cristian-13-dev <cebotaricristian92@gmail.com>
2026-01-07 13:09:45 +0200

    Chore(env): Change supabase client env variable name

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,8 +1,6 @@
 import { createBrowserClient } from "@supabase/ssr";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseKey =
-  process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY ??
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = process.env.SUPABASE_URL!;
+const supabaseKey = process.env.SUPABASE_ANON_KEY!;
 
 export const supabase = createBrowserClient(supabaseUrl, supabaseKey);
